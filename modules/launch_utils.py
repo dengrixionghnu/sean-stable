40d7f365f74d9929cc58c244aa0a13b6d5e0b9ee
dengrixionghnu <32235642+dengrixionghnu@users.noreply.github.com>
2023-09-18 22:05:44 +0800

    Update launch_utils.py

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -287,7 +287,7 @@ def prepare_environment():
     stable_diffusion_commit_hash = os.environ.get('STABLE_DIFFUSION_COMMIT_HASH', "cf1d67a6fd5ea1aa600c4df58e5b47da45f6bdbf")
     stable_diffusion_xl_commit_hash = os.environ.get('STABLE_DIFFUSION_XL_COMMIT_HASH', "5c10deee76adad0032b412294130090932317a87")
     k_diffusion_commit_hash = os.environ.get('K_DIFFUSION_COMMIT_HASH', "c9fe758757e022f05ca5a53fa8fac28889e4f1cf")
-    codeformer_commit_hash = os.environ.get('CODEFORMER_COMMIT_HASH', "c5b4593074ba6214284d6acd5f1719b6c5d739af")
+    codeformer_commit_hash = os.environ.get('CODEFORMER_COMMIT_HASH', "8392d0334956108ab53d9439c4b9fc9c4af0d66d")
     blip_commit_hash = os.environ.get('BLIP_COMMIT_HASH', "48211a1594f1321b00f14c9f7a5b4813144b2fb9")
 
     try:
